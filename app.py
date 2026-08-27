cb4d6afef9b09f32fd53925e9e6e34d9ae6364fe
Usman-raza05 <razau8993@gmail.com>
2026-08-27 10:44:11 +0530

    Initial commit for STUDY Assistant

--- app.py
+++ app.py
@@ -111,5 +111,9 @@ def process_pdf():
         print("Backend Error:", str(e))
         return jsonify({'error': f"AI request failed: {str(e)}"}), 502
 
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", debug=True, port=port)
